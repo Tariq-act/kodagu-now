f1c5c507062024812427a3724e3e5f61bccf64b6
Tariq-act <khantarikact@gmail.com>
2023-07-18 17:32:04 +0530

    latest commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,13 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: '#4D5E80', // Custom primary color
+        secondary: '#7D8FB3', // Custom secondary color
+        backgroundColor: '#F7F8FA',
+        // Add more custom colors as needed
+      },
     },
   },
   plugins: [],
-}
+};
